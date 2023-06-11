12526ecf45e217ede73276590ebb91d6c92f89e5
dusixx <113414935+dusixx@users.noreply.github.com>
2023-06-12 00:32:30 +0300

    small fix

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,13 @@
 import { LinkButton } from 'styles/shared';
 import { Caption, Container, Text } from './ErrorMessage.styled';
+import { BiMessageRoundedError as IconError } from 'react-icons/bi';
 
 export const ErrorMessage = ({ error }) => {
   console.error(error);
 
   return (
     <Container>
+      <IconError size={40} color="var(--color-blue)" />
       <Caption>Something went wrong</Caption>
       <Text>{error.code}</Text>
       <LinkButton to={-1} style={{ textTransform: 'capitalize' }}>
--- src/components/ErrorMessage/PageNotFound.jsx
+++ src/components/ErrorMessage/PageNotFound.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { LinkButton } from 'styles/shared';
 import { Caption, Container, Text } from './ErrorMessage.styled';
+import { BiMessageRoundedError as IconError } from 'react-icons/bi';
 
 const TEXT =
   'It may have been moved, or you may have simply mistyped the page address.';
@@ -11,7 +12,8 @@ export const PageNotFound = () => {
 
   return (
     <Container>
-      <Caption>Page not found</Caption>
+      <IconError size={40} color="var(--color-blue)" />
+      <Caption>404: Page not found</Caption>
       <Text>{TEXT}</Text>
       <LinkButton to="/" style={{ textTransform: 'capitalize' }}>
         Go to the homepage
